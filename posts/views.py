b4d34d6eac8fd8ed31dfa619e94a2561dff7f40a
zahra imen <zahraimen91@gmail.com>
2023-08-14 18:57:24 +0330

    add view level for permission

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,16 @@
 
-from rest_framework import generics
+from rest_framework import generics,permissions
 
 from .models import Post
 from .serializers import PostSerializer
 
 # Create your views here.
 class PostListView(generics.ListCreateAPIView):
+    permission_classes=(permissions.IsAuthenticated,)
     queryset=Post.objects.all()
     serializer_class=PostSerializer
     
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes=(permissions.IsAuthenticated,)
     queryset=Post.objects.all()
     serializer_class=PostSerializer
