8ec2c84d60b53efe65d62e0de14bd348033d074a
zahra imen <zahraimen91@gmail.com>
2023-08-15 14:34:54 +0330

    add authtoken into installed app and set new config for authentication

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     
     #3rd party
     'rest_framework',
+    'rest_framework.authtoken',
     
     #local
     'posts',
@@ -49,7 +50,11 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
 'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+'DEFAULT_AUTHENTICATION_CLASSES': [
+    'rest_framework.authentication.SessionAuthentication',
+    'rest_framework.authentication.TokenAuthentication',
+],
 }
 
 
