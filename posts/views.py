67cbb11dc9d77accb1f174919abd3a3ce82110da
zahra imen <zahraimen91@gmail.com>
2023-08-16 12:41:18 +0330

    create view fol list and detali view of users

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,10 @@
 
+from django.contrib.auth import get_user_model
 from rest_framework import generics
 
 from .permissions import IsAuthorOrReadOnly
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer,UserSerializer
 
 # Create your views here.
 class PostListView(generics.ListCreateAPIView):
@@ -14,3 +15,11 @@ class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes=(IsAuthorOrReadOnly,)
     queryset=Post.objects.all()
     serializer_class=PostSerializer
+
+class UserListView(generics.ListCreateAPIView):
+    queryset=get_user_model.objects.all()
+    serializer_class = UserSerializer
+    
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset=get_user_model.objects.all()
+    serializer_class=UserSerializer
