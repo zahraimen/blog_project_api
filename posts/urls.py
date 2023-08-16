dbf8948183b268e4ce7ddd06a580a817b098ea82
zahra imen <zahraimen91@gmail.com>
2023-08-16 13:31:07 +0330

    add userviews to rotes

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import PostListView,PostDetailView
+from . import views
 
 urlpatterns = [
-    path('',PostListView.as_view()),
-    path('<int:pk>/',PostDetailView.as_view()),
+    path('',views.PostListView.as_view()),
+    path('<int:pk>/',views.PostDetailView.as_view()),
+    path('users/',views.UserListView.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
+    
 ]
